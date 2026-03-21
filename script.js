6d3fff705f64a8d9e32dc47b58760e42ba82d2a6
valstike <acab.slo@proton.me>
2026-03-21 12:15:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,87 @@ document.querySelector("form").addEventListener("submit", function(e) {
             });
         }
     });
+
+    class TypewriterEffect {
+    constructor(element, texts, options = {}) {
+        this.element = element;
+        this.texts = Array.isArray(texts) ? texts : [texts];
+        this.typingSpeed = options.typingSpeed || 8;
+        this.deletingSpeed = options.deletingSpeed || 10;
+        this.pauseTime = options.pauseTime || 1500;
+        this.animationType = options.animationType || 'typeDeleteLoop';
+        this.currentTextIndex = 0;
+        this.currentCharIndex = 0;
+        this.isDeleting = false;
+        this.displayText = '';
+        
+        this.start();
+    }
+    
+    start() {
+        this.type();
+    }
+    
+    type() {
+        const currentText = this.texts[this.currentTextIndex];
+        
+        if (!this.isDeleting) {
+            if (this.currentCharIndex < currentText.length) {
+                this.displayText += currentText.charAt(this.currentCharIndex);
+                this.currentCharIndex++;
+            } else {
+                if (this.animationType === 'typeDelete' || this.animationType === 'typeDeleteLoop') {
+                    setTimeout(() => { this.isDeleting = true; this.type(); }, this.pauseTime);
+                    return;
+                } else if (this.animationType === 'typeLoop' || this.animationType === 'typeDeleteLoop') {
+                    if (this.currentTextIndex < this.texts.length - 1) {
+                        this.currentTextIndex++;
+                        this.currentCharIndex = 0;
+                        this.displayText += '\n';
+                    } else if (this.animationType === 'typeLoop') {
+                        this.currentTextIndex = 0;
+                        this.currentCharIndex = 0;
+                        this.displayText = '';
+                    }
+                } else {
+                    if (this.currentTextIndex < this.texts.length - 1) {
+                        this.currentTextIndex++;
+                        this.currentCharIndex = 0;
+                        this.displayText += '\n';
+                    } else {
+                        this.element.innerHTML = this.displayText.replace(/\n/g, '<br>');
+                        return;
+                    }
+                }
+            }
+        } else {
+            if (this.displayText.length > 0) {
+                this.displayText = this.displayText.slice(0, -1);
+            } else {
+                this.isDeleting = false;
+                if (this.animationType === 'typeDeleteLoop') {
+                    this.currentTextIndex = (this.currentTextIndex + 1) % this.texts.length;
+                    this.currentCharIndex = 0;
+                }
+            }
+        }
+        
+        this.element.innerHTML = this.displayText.replace(/\n/g, '<br>');
+        
+        const speed = this.isDeleting ? 1000 / this.deletingSpeed : 1000 / this.typingSpeed;
+                        setTimeout(() => this.type(), speed);
+    }
+}
+
+// Initialize the typewriter effect
+document.addEventListener('DOMContentLoaded', function() {
+    const element = document.getElementById('typewriter-text');
+    const texts = ["Defense is a system, not a moment."];
+    
+    new TypewriterEffect(element, texts, {
+        typingSpeed: 8,
+        deletingSpeed: 10,
+        pauseTime: 1500,
+        animationType: 'typeDeleteLoop'
+    });
+});
